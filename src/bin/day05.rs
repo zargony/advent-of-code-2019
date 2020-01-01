88fd47265a63ce99d0eafcaa40570fbba5df5f1c
Andreas Neuhaus <zargony@zargony.com>
2020-01-01 20:10:13 +0100

    Day 5 part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -6,7 +6,12 @@ async fn main() -> io::Result<()> {
     let mut memory = Memory::from_day(5).await?;
     let mut vm = Vm::new(&mut memory);
     vm.input(&[1]).run();
-    println!("TEST output: {:?}", vm.output());
+    println!("TEST diagnostic output: {:?}", vm.output());
+
+    let mut memory = Memory::from_day(5).await?;
+    let mut vm = Vm::new(&mut memory);
+    vm.input(&[5]).run();
+    println!("TEST diagnostic code for system ID 5: {}", vm.output()[0]);
 
     Ok(())
 }
